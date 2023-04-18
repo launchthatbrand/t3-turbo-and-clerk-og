44df60ab2a9eb3f6b4214a9091fa2f9d92f73b2e
LaunchThatBrand <info@launchthatbrand.com>
2023-04-18 16:03:58 -0400

    init modifications. prisma, dev commands, prisma schema

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,6 +1,6 @@
-import { ExpoConfig, ConfigContext } from "@expo/config";
+import { ConfigContext, ExpoConfig } from "@expo/config";
 
-const CLERK_PUBLISHABLE_KEY = "your-clerk-publishable-key";
+const CLERK_PUBLISHABLE_KEY = "pk_test_d29ydGh5LXdhaG9vLTk0LmNsZXJrLmFjY291bnRzLmRldiQ";
 
 const defineConfig = (_ctx: ConfigContext): ExpoConfig => ({
   name: "expo",
